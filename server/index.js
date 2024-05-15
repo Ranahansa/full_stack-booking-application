03a0949925799059d77bdee90af2590375932fd5
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-05-15 23:21:15 +0530

    Add error handling middleware and createError utility function

--- server/index.js
+++ server/index.js
@@ -25,7 +25,12 @@ app.use("/api/auth", auth);
 app.use ((err, req, res, next) => {
     const errorStatus = err.status || 500;
     const errorMessage = err.message || "Something went wrong";
-    res.status(errorStatus).send(errorMessage);
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    })
 })
 
 connectDB();
