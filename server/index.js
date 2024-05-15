a3cef34d812b8a8428758dca876d3d78dd919c34
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-05-15 22:32:31 +0530

    Add server configuration files, routes, and models

--- server/index.js
+++ server/index.js
@@ -2,12 +2,31 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./lib/db.js";
+import users from "./routes/users.js";
+import hotels from "./routes/hotels.js";
+import rooms from "./routes/rooms.js";
+import auth from "./routes/auth.js";
+
 
 const app = express();
 const port = process.env.PORT || 8800;
 
+
+
+//middleware
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/users", users);
+app.use("/api/hotels", hotels);
+app.use("/api/rooms", rooms);
+app.use("/api/auth", auth);
+
+app.use ((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong";
+    res.status(errorStatus).send(errorMessage);
+})
 
 connectDB();
 
