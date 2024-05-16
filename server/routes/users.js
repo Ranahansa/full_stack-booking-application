c83454b14b4b4374297214630eb89956c2fb6ac1
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-05-16 17:05:22 +0530

    Add admin authentication to user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { deleteUser, getAllUsers, getUser, updateUser } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
@@ -9,12 +9,18 @@ router.get("/checkauthentication", verifyToken, (req, res, next) => {
     res.send("hello user, you are logged in");
 });
 
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.get("/:id", getUser);
-router.get("/", getAllUsers);
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+    res.send("hello user, you are authenticated");
+})
 
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+    res.send("hello admin, you are authenticated");
+})
 
+router.put("/:id",verifyUser, updateUser);
+router.delete("/:id",verifyUser, deleteUser);
+router.get("/:id",verifyUser, getUser);
+router.get("/",verifyAdmin, getAllUsers);
 
 
 export default router;
